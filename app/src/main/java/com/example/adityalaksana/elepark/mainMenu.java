3fc30729202b493b6f4a0806b477726fbe0ff1cd
raufimusaddiq <raufimusaddiq@gmail.com>
2016-09-09 13:28:52 +0700

    Tambah Tombol

--- app/src/main/java/com/example/adityalaksana/elepark/mainMenu.java
+++ app/src/main/java/com/example/adityalaksana/elepark/mainMenu.java
@@ -1,7 +1,10 @@
 package com.example.adityalaksana.elepark;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class mainMenu extends AppCompatActivity {
 
@@ -9,6 +12,14 @@ public class mainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        ImageButton gotoset = (ImageButton) findViewById(R.id.imageButtonSet);
+        gotoset.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent setIntent = new Intent(view.getContext(), FormActivity.class);
+            }
+        });
     }
 
 
