33c68a91c0864b3f8d8355d467143beb32275180
raufimusaddiq <raufimusaddiq@gmail.com>
2016-09-10 01:04:42 +0700

    Tambah Main Menu

--- app/src/main/java/com/example/adityalaksana/elepark/mainMenu.java
+++ app/src/main/java/com/example/adityalaksana/elepark/mainMenu.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class mainMenu extends AppCompatActivity {
     String floor, block;
@@ -12,6 +13,15 @@ public class mainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        final Intent intent = getIntent();
+        floor = intent.getStringExtra("homefloor");
+        block = intent.getStringExtra("homeblock");
+
+        TextView f = (TextView) findViewById(R.id.textViewHomeFloor);
+        f.setText(floor);
+        TextView t = (TextView) findViewById(R.id.textViewHomeBlock);
+        t.setText(block);
 
         ImageButton gotoset = (ImageButton) findViewById(R.id.imageButtonSet);
         gotoset.setOnClickListener(new View.OnClickListener(){
@@ -27,6 +37,8 @@ public class mainMenu extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent findIntent = new Intent(mainMenu.this, FindLocation.class);
+                intent.putExtra("floor", floor);
+                intent.putExtra("block", block);
                 startActivity(findIntent);
             }
         });
